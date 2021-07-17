236e3bcea854f3e05032ac333e377af99f244f93
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-17 07:43:52 -0300

    add styled componentes

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+import { Container, Title } from './styles';
+ 
+export default function Dashboard(): ReactElement {
+  return (
+    <Container>
+      <Title>
+        Dashboard
+      </Title>
+    </Container>
+  );
+}
+ 
