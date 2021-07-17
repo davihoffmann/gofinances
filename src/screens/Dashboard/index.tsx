b7264d62711e35d89eec27a576eff092b20e576c
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-17 15:17:06 -0300

    create header component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,30 @@
 import React, { ReactElement } from 'react';
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './styles';
  
 export default function Dashboard(): ReactElement {
   return (
     <Container>
-      <Title>
-        Dashboard
-      </Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://github.com/davihoffmann.png'}} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Davi</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+        
+      </Header>
     </Container>
   );
 }
