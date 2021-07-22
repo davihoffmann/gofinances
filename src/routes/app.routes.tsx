397c243eda05e435cb9914120e0ca34e78688197
Davi Hoffmann <davi.phoffmann@gmail.com>
2021-07-22 17:05:26 -0300

    add style to tab navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,59 @@
 import React, { ReactElement } from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import DashBoard from '../screens/Dashboard';
 import Register from '../screens/Register';
+
+import theme from '../global/styles/theme';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export default function AppRoutes(): ReactElement {
   return (
-    <Navigator>
-      <Screen name="DashBoard" component={DashBoard} />
-      <Screen name="Register" component={Register} />
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.secondary,
+        inactiveTintColor: theme.colors.text,
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 80,
+        },
+      }}
+    >
+      <Screen
+        name="DashBoard"
+        component={DashBoard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Register"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
