ac1a81deb27a7aea3e3ce3d8f7af42eb34b2a195
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-07-27 09:22:40 -0300

    create social button component

---
+++ src/components/SignInSocialButton/index.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react';
+import { RectButtonProps } from 'react-native-gesture-handler';
+import { SvgProps } from 'react-native-svg';
+
+import { Button, ImageContainer, Text } from './styles';
+
+
+interface Props extends RectButtonProps {
+  title: string;
+  svg: React.FC<SvgProps>
+}
+ 
+export default function SignInSocialButton({ title, svg: Svg, ...rest }: Props): ReactElement {
+  return (
+    <Button {...rest}>
+      <ImageContainer>
+        <Svg />
+      </ImageContainer>
+
+      <Text>{title}</Text>
+    </Button>
+  );
+}
+ 
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -5,6 +5,8 @@ import AppleSvg from '../../assets/apple.svg';
 import GoogleSvg from '../../assets/google.svg';
 import LogoSvg from '../../assets/logo.svg';
 
+import SignInSocialButton from '../../components/SignInSocialButton';
+
 import {
   Container,
   Header,
@@ -12,6 +14,7 @@ import {
   Title,
   SignInTitle,
   Footer,
+  FooterWrapper
 } from './styles';
 
 export default function SignIn(): ReactElement {
@@ -34,7 +37,12 @@ export default function SignIn(): ReactElement {
         </SignInTitle>
       </Header>
 
-      <Footer></Footer>
+      <Footer>
+        <FooterWrapper>
+          <SignInSocialButton title="Entrar com Google" svg={GoogleSvg} />
+          <SignInSocialButton title="Entrar com Apple" svg={AppleSvg} />
+        </FooterWrapper>
+      </Footer>
     </Container>
   );
 }
