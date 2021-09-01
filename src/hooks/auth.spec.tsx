af4ebb054433ba6495fd554c8a38548f3bc0dafc
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-09-01 17:07:59 -0300

    desenvolvimento de mock especifico para cada test

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -12,13 +12,34 @@ fetchMock.enableMocks();
 
 describe('Auth Hook', () => {
   it('Should be able to sign in with Google Account', async () => {
-
     //Primeiro, nós precisamos do Token. Então, vamos Mockar a função de startAsync.
     const googleMocked = mocked(startAsync as any);
     googleMocked.mockReturnValueOnce({
       type: 'success',
       params: {
         access_token: 'any_token',
+      }
+    });
+
+    //Agora que temos o Token, vamos mockar a requisição ttp dos dados de profile do usuário.
+    fetchMock.mockResponseOnce(JSON.stringify({}));
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).toBeTruthy();
+  });
+
+  it('User should not connect if cancel authentication with Google', async () => {
+    //Primeiro, nós precisamos do Token. Então, vamos Mockar a função de startAsync.
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValueOnce({
+      type: 'cancel',
+      params: {
+        access_token: '',
       }
     });
 
@@ -38,9 +59,6 @@ describe('Auth Hook', () => {
 
     await act(() => result.current.signInWithGoogle());
 
-    // Você até pode usar esse console.log para visualizar os dados.
-    console.log("USER PROFILE =>", result.current.user);
-
-    expect(result.current.user).toBeTruthy();
+    expect(result.current.user).not.toHaveProperty('id');
   })
 })
